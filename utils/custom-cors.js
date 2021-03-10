211c49dc2e4f45fac19bccac5390669d9ec8f3c3
Bernhard Paolo <bernhardpaolocena@gmail.com>
2021-03-11 00:13:33 +0800

    added delete in cors policy

--- utils/custom-cors.js
+++ utils/custom-cors.js
@@ -2,7 +2,7 @@ const corsSettings = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Expose-Headers", "Content-Length");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,DELETE,POST,PUT");
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin,Authorization,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method"
